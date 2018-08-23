9a014640e5819557a3bd62101a67262f2f0fbcd6
Matl57 <42205811+Matl57@users.noreply.github.com>
2018-08-23 10:02:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function play(connection, message) {
 client.on("ready", () => {
 
     console.log("Je suis prêt !");
-    client.user.setGame("d*help");
+    client.user.setGame("d*help | version bêta""DalBot By Tutal");
     
 
 });
