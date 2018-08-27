d8b3a71fa2b2a5156894ff485562cbc057102a5a
Matl57 <42205811+Matl57@users.noreply.github.com>
2018-08-27 20:21:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function play(connection, message) {
 client.on("ready", () => {
 
     console.log("Je suis prêt !");
-    client.user.setGame(`d*help | version bêta, ${bot.ping} `);
+    client.user.setGame(`d*help | version bêta, ${client.user.size}`);
     
 
 });
